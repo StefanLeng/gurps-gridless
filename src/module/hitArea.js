a10426d5711c89e9e61ddfc35baba79aa28d43a9
StefanLeng <stefan.leng@gmx.de>
2025-01-24 12:36:20 +0100

    hitArea partial

--- src/module/hitArea.js
+++ src/module/hitArea.js
@@ -1,16 +1,90 @@
 //import { MODULE_ID } from './constants.js';
 import { getDirectionFromAbautFace } from './rangeindicator.js';
+import { isHexRowGrid, isHexGrid } from './token.js';
+import { MODULE_ID } from './constants.js';
 
-export function drawHitArea(token) {
+function rectangelHitArea(token) {
   const { w: width, h: height } = token;
-  const { anchorX, anchorY, scaleY, scaleX } = token.document.texture;
-  const points = [
+  return [
     new PIXI.Point(0, 0), //eslint-disable-line no-undef
     new PIXI.Point(width, 0), //eslint-disable-line no-undef
     new PIXI.Point(width, height), //eslint-disable-line no-undef
     new PIXI.Point(0, height), //eslint-disable-line no-undef
   ];
+}
 
+function hexLongBodyShape(token) {
+  const { w: widthpx, h: heightpx } = token;
+  const w = (isHexRowGrid() ? canvas.grid.sizeY : canvas.grid.sizeX) / 2;
+  const wHalf = w / 2;
+  const h = (isHexRowGrid() ? canvas.grid.sizeX : canvas.grid.sizeY) / 2;
+  const width = token.document.flags[MODULE_ID]?.tokenWidth;
+  const height = token.document.flags[MODULE_ID]?.tokenLength;
+  let y = height;
+  let posX = width % 2 === 1 ? wHalf : isHexRowGrid() ? w + wHalf : w + wHalf / Math.sqrt(3);
+  let down = false;
+  let points = [new PIXI.Point(posX + widthpx / 2, h * y + heightpx / 2)]; //eslint-disable-line no-undef
+  while (y > -height) {
+    if (y > height - Math.ceil((width + 1) / 2)) {
+      if (down) {
+        y = y - 1;
+        posX = posX - wHalf;
+      } else {
+        posX = posX - w;
+      }
+    } else if (y > -height + Math.ceil((width + 1) / 2)) {
+      y = y - 1;
+      if (down) {
+        posX = posX - wHalf;
+      } else {
+        posX = posX + wHalf;
+      }
+    } else {
+      if (!down) {
+        y = y - 1;
+        posX = posX + wHalf;
+      } else {
+        posX = posX + w;
+      }
+    }
+    down = !down;
+    points.push(new PIXI.Point(posX + widthpx / 2, h * y + heightpx / 2)); //eslint-disable-line no-undef
+  }
+  while (y < height) {
+    if (y < -height + Math.ceil(width / 2)) {
+      if (!down) {
+        y = y + 1;
+        posX = posX + wHalf;
+      } else {
+        posX = posX + w;
+      }
+    } else if (y < height - Math.ceil(width / 2)) {
+      y = y + 1;
+      if (down) {
+        posX = posX - wHalf;
+      } else {
+        posX = posX + wHalf;
+      }
+    } else {
+      if (down) {
+        y = y + 1;
+        posX = posX - wHalf;
+      } else {
+        posX = posX - w;
+      }
+    }
+    down = !down;
+    points.push(new PIXI.Point(posX + widthpx / 2, h * y + heightpx / 2)); //eslint-disable-line no-undef
+  }
+
+  return points;
+}
+
+export function drawHitArea(token) {
+  const points = isHexGrid() ? hexLongBodyShape(token) : rectangelHitArea(token);
+
+  const { w: width, h: height } = token;
+  const { anchorX, anchorY, scaleY, scaleX } = token.document.texture;
   const tokenDirectionDegree = getDirectionFromAbautFace(token);
   const tokenDirection = (tokenDirectionDegree / 180) * Math.PI;
 
